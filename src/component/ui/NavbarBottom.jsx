2da9cb3c20970eb4bb2d7dae586f93e1891c7427
lutfi aulia sidik <biolutfi@gmail.com>
2024-02-07 15:54:53 +0700

    menambahkan link dari react-router-dom

--- src/component/ui/NavbarBottom.jsx
+++ src/component/ui/NavbarBottom.jsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom'
 import { BsHouse, BsSearch, BsChatSquareDots, BsPersonCircle } from "react-icons/bs"
 
-const MenuItem = ({ icon, label }) =>
+const MenuItem = ({ icon, label, link }) =>
 {
   return (
-    <li className="flex gap-1.5 flex-col justify-center text-slate-50/50 w-full h-16 text-xs text-center capitalize hover:font-bold hover:text-sky-500 hover:bg-slate-800/70 transition ease-in-out duration-[.3s]">
-      <p className="mx-auto text-[22px]">{ icon }</p>
-      <p>{ label }</p>
-    </li>
+    <Link to={ link } className="w-full">
+      <li className="flex gap-1.5 flex-col justify-center text-slate-50/50 w-full h-16 text-xs text-center capitalize hover:font-bold hover:text-sky-500 hover:bg-slate-800/70 transition ease-in-out duration-[.3s]">
+        <p className="mx-auto text-[22px]">{ icon }</p>
+        <p>{ label }</p>
+      </li>
+    </Link>
   )
 }
 
@@ -15,8 +18,8 @@ const NavbarBottom = () =>
   return (
     <nav className="fixed bottom-0 w-full h-16 bg-slate-950/50 backdrop-blur-md">
       <ul className="flex items-center justify-center">
-        <MenuItem label="home" icon={<BsHouse />}/>
-        <MenuItem label="telusuri" icon={<BsSearch />}/>
+        <MenuItem label="home" icon={<BsHouse />} link='/' />
+        <MenuItem label="telusuri" icon={<BsSearch />} />
         <MenuItem label="pesan" icon={<BsChatSquareDots />}/>
         <MenuItem label="profile" icon={<BsPersonCircle />}/>
       </ul>
