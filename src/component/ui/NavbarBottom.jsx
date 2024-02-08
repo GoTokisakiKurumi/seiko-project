b89cbdbda88b719c0db3e32ab266a4ec92d2707d
lutfi aulia sidik <biolutfi@gmail.com>
2024-02-08 18:45:56 +0700

    menambahkan fitur active di navbar bottom

--- src/component/ui/NavbarBottom.jsx
+++ src/component/ui/NavbarBottom.jsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { BsHouse, BsSearch, BsChatSquareDots, BsPersonCircle } from "react-icons/bs"
 
 const MenuItem = ({ icon, label, link }) =>
 {
   return (
-    <Link to={ link } className="w-full">
-      <li className="flex gap-1.5 flex-col justify-center text-slate-50/50 w-full h-16 text-xs text-center capitalize hover:font-bold hover:text-sky-500 hover:bg-slate-800/70 transition ease-in-out duration-[.3s]">
+    <NavLink to={ link } className={({ isActive }) => (isActive ? 'group w-full bg-slate-800/90' : 'w-full' )}>
+      <li className="group-first:text-sky-500 group-first:font-bold flex gap-1.5 flex-col justify-center text-slate-50/50 w-full h-16 text-xs text-center capitalize hover:font-bold hover:text-sky-500 hover:bg-slate-800/70 transition ease-in-out duration-[.3s]">
         <p className="mx-auto text-[22px]">{ icon }</p>
         <p>{ label }</p>
       </li>
-    </Link>
+    </NavLink>
   )
 }
 
@@ -19,9 +19,9 @@ const NavbarBottom = () =>
     <nav className="fixed bottom-0 w-full h-16 bg-slate-950/50 backdrop-blur-md">
       <ul className="flex items-center justify-center">
         <MenuItem label="home" icon={<BsHouse />} link='/' />
-        <MenuItem label="telusuri" icon={<BsSearch />} />
-        <MenuItem label="pesan" icon={<BsChatSquareDots />}/>
-        <MenuItem label="profile" icon={<BsPersonCircle />}/>
+        <MenuItem label="telusuri" icon={<BsSearch />} link='/telusuri' />
+        <MenuItem label="pesan" icon={<BsChatSquareDots />} link='/pesan' />
+        <MenuItem label="profile" icon={<BsPersonCircle />} link='/profile' />
       </ul>
     </nav>
   )
